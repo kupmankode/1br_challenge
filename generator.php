2319c68d16ba4e20c4d8c8280e825da6c535371f
Kenny Tran <trankennykhang@hotmail.com>
2024-04-10 08:29:28 +0930

    code improvement

--- generator.php
+++ generator.php
@@ -1,48 +1,58 @@
 <?php
-$handle = fopen('data/weather_stations.csv', 'r');
+/**
+ * Requirements:
+ * - Read weather from file
+ * - Find the max and min for each station
+ * - Order by alphabet
+ * - Output the result
+ */
+
+ // Check CLI
+if (PHP_SAPI != "cli") {
+    die('Not a command');
+}
+// Get args
+if (count($argv) != 2) {
+    die('Invalid command');
+}
+$file = 'data/'.$argv[1];
+if (!file_exists($file)){
+    die('Can\'t locate the file');
+}
+
+// Pass all basic checks, continue...
+$time = time();
+print date('c', time()).PHP_EOL;
+print "Memory usage in MB: ".(memory_get_usage() / (1024 * 1024)).PHP_EOL;
+
+$handle = fopen($file, 'r');
 // loadd all station names
 $stations = [];
 if ($handle != false) {
-    fgets($handle, 1000);
-    fgets($handle, 1000);
     while(($data = fgets($handle, 1000)) != false) {
         $arr = explode(';', $data);
         if (count($arr) == 2) {
-            $stations[] = $arr[0];
+            $arr[1] = (int)$arr[1];
+            if (!isset($stations[$arr[0]])) {
+                $stations[$arr[0]] = [];
+                $stations[$arr[0]][0] = $arr[1];
+                $stations[$arr[0]][1] = $arr[1];
+            } else {
+                if ($stations[$arr[0]][0] > $arr[1])
+                    $stations[$arr[0]][0] = $arr[1];
+                elseif ($stations[$arr[0]][1] < $arr[1])
+                    $stations[$arr[0]][1] = $arr[1];
+            }
         }
     }
-}
-if (empty($stations))
-    die('Check station template file');
-//get number of rows from command line
-$rows = 1000000000;
-
-//set temperature range
-$temp_range = [-99, 99];
-$time = time();
-print date('c', time()).PHP_EOL;
-print "Use generator to write ".number_format($rows)." rows".PHP_EOL;
-print "Memory usage in MB: ".(memory_get_usage() / (1024 * 1024)).PHP_EOL;
-
-// if less than millions, use array otherwise use generator
-if ($rows < 1000000001) {
-    $arr = build_weather_data($stations, $temp_range, $rows);
     $str = "";
-    foreach ($arr as $ar) {
-        $str .= $ar."\n";
+    foreach ($stations as $name=>$values) {
+        $med = round(($values[0] + $values[1]) / 2, 2);
+        $str .= $name. ': '. $values[0] . '/'. $med . '/' . $values[1]. ';';
     }
-    print "Memory usage to process array and store string in MB: ".(memory_get_usage() / (1024 * 1024)).PHP_EOL;
-    print date('c', time()).PHP_EOL;
-    file_put_contents('data/result.csv', $str);
-    
-    
+    print $str . PHP_EOL;
+    print "Hash string: ". md5($str). PHP_EOL;
 }
 print "Execute time is: ". (time() - $time) . PHP_EOL;
+print "Memory usage in MB: ".(memory_get_usage() / (1024 * 1024)).PHP_EOL;
 print date('c', time()).PHP_EOL;
-/////////////////////////////////////////////////////
-function build_weather_data($stations, $temp_range, $rows) {
-    $total = count($stations) - 1;
-    for($i=0;$i<$rows;$i++) {
-        yield $stations[rand(0,$total)].';'.rand($temp_range[0], $temp_range[1]) + (rand(1,99) / 100);
-    }
-}
